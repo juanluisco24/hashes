58a2f77c8363c275a816b2d3254d130d643bc104
Juan Luis Cabello <juan.luiscoropeza@gmail.com>
2020-12-08 23:24:08 -0300

    first commit

---
+++ desafios_online/filtrando_hash.rb
@@ -0,0 +1,21 @@
+ventas = {
+    Octubre: 65000,
+    Noviembre: 68000,
+    Diciembre: 72000
+    }
+
+    def filter(hash)
+        filtered_hash = {}
+            hash.each do |k,v|
+                if v < 70000
+                filtered_hash[k] = v
+            end
+        end
+        puts filtered_hash 
+    end
+
+    filter({
+        Octubre: 65000,
+        Noviembre: 68000,
+        Diciembre: 72000
+        })
