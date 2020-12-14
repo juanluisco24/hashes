98d485158c2e9f09d865d2cf427b4a5cb6e30a27
Juan Luis Cabello <juan.luiscoropeza@gmail.com>
2020-12-14 18:51:27 -0300

    desafio de lunes

---
+++ desafios_lunes/trimestres.rb
@@ -0,0 +1,24 @@
+ventas = {
+    Enero: 15000,
+    Febrero: 22000,
+    Marzo: 12000,
+    Abril: 17000,
+    Mayo: 81000,
+    Junio: 13000,
+    Julio: 21000,
+    Agosto: 41200,
+    Septiembre: 25000,
+    Octubre: 21500,
+    Noviembre: 91000,
+    Diciembre: 21000
+    }
+
+valores = ventas.values
+
+valores_quarter = valores.each_slice(3).to_a
+
+sum_quarter = valores_quarter.map {|x| x.sum}
+
+array = ['Q1', 'Q2', 'Q3', 'Q4']
+
+print array.zip(sum_quarter).to_h
